adcee4236eddf92cea6f4a6da76d6c1eb4cd4239
Yaël Balbastre <yael.balbastre@gmail.com>
2025-08-07 14:07:21 +0100

    Create inject_service_worker.py

---
+++ scripts/inject_service_worker.py
@@ -0,0 +1,40 @@
+from argparse import ArgumentParser
+from bs4 import BeautifulSoup
+
+code = """
+if ('serviceWorker' in navigator) {
+    const scope = location.pathname.replace(/\/[^\/]+$/, '/');
+    navigator.serviceWorker.register('sw.js', { scope })
+             .then(function(reg) {
+                 reg.addEventListener('updatefound', function() {
+                     const installingWorker = reg.installing;
+                     console.log('A new service worker is being installed:',
+                                 installingWorker);
+                 });
+                 // registration worked
+                 console.log('Registration succeeded. Scope is ' + reg.scope);
+             }).catch(function(error) {
+                 // registration failed
+                 console.log('Registration failed with ' + error);
+             });
+}
+"""
+
+p = ArgumentParser("Inject service worker into a html file.")
+p.add_argument('-i', '--input', default='index.html')
+p.add_argument('-o', '--output', default=None)
+p.add_argument('-s', '--script', default='sw.js')
+args = p.parse()
+
+code = code.replace('sw.js', args.script)
+
+with open(args.input, "rt") as f:
+  html = BeautifulSoup(f.read())
+
+meta = html.find('meta')
+script = html.new_tag('script')
+script.append(code)
+meta.insert(0, script)
+
+with open(args.output or args.input, "wt") a f:
+  f.write(html.prettify())
