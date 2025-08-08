636c56adc04727643b174dcd01a3e5027c2b2647
Yaël Balbastre <yael.balbastre@gmail.com>
2025-08-08 17:18:01 +0100

    [FEAT] Service worker that automatically authenticates LINC & DANDI datasources (#1)

---
+++ scripts/inject_service_worker.py
@@ -0,0 +1,131 @@
+from argparse import ArgumentParser
+from bs4 import BeautifulSoup
+
+code_head = """
+<script>
+if ('serviceWorker' in navigator) {
+    const scope = location.pathname.replace(/\/[^\/]+$/, '/');
+    navigator.serviceWorker.register('/ngapp/sw.js', { scope: '/ngapp/', type: 'module' })
+             .then(function(reg) {
+                 reg.addEventListener('updatefound', function() {
+                     const installingWorker = reg.installing;
+                     console.log('A new service worker is being installed:',
+                                 installingWorker);
+                 });
+                 // registration worked
+                 console.log('Registration succeeded. Scope is ' + reg.scope);
+             }).catch(function(error) {
+                 // registration failed
+                 console.log('Registration failed with ' + error);
+             });
+}
+</script>
+"""
+
+code_body = """
+<dialog id="dandi-auth-dialog">
+  <form method="dialog">
+    <p>
+      <label id="dandi-auth-prompt">
+        <span>Enter your <b>{instance}</b> Token: </span>
+        <input type="text" required />
+      </label>
+    </p>
+    <div>
+      <button id="dandi-auth-cancel">Cancel</button>
+      <button id="dandi-auth-ok">OK</button>
+    </div>
+  </form>
+</dialog>
+
+<script>
+const channel_dandi_auth = new BroadcastChannel('channel-dandi-auth');
+let dandi_prompting = false;
+let dandi_message_received = true;
+
+// setup dialog box
+const dandiDialog = document.getElementById("dandi-auth-dialog");
+const dandiLabel = document.getElementById("dandi-auth-prompt");
+const dandiOK = dandiDialog.querySelector("#dandi-auth-ok");
+const dandiCancel = dandiDialog.querySelector("#dandi-auth-cancel");
+const dandiToken = dandiLabel.getElementsByTagName("input")[0];
+const dandiInstance = dandiLabel.getElementsByTagName("span")[0].getElementsByTagName("b")[0];
+
+dandiDialog.addEventListener("close", (e) => {
+  dandi_prompting = false;
+});
+
+dandiOK.addEventListener("click", (e) => {
+  dandiDialog.close();
+  dandi_message_received = false;
+  const instance = dandiInstance.textContent.toLocaleLowerCase();
+  const token = dandiToken.value;
+  if (token) {
+    channel_dandi_auth.postMessage({
+      instance: instance,
+      token: token,
+      header: { Authorization: "token " + token }
+    });
+  } else {
+    channel_dandi_auth.postMessage({
+      instance: instance,
+      token: undefined,
+    });
+  }
+});
+
+dandiCancel.addEventListener("click", (e) => {
+  dandiDialog.close();
+  dandi_message_received = false;
+  const instance = dandiInstance.textContent.toLocaleLowerCase();
+  channel_dandi_auth.postMessage({
+    instance: instance,
+    token: undefined,
+  });
+});
+
+// two-way communication
+channel_dandi_auth.onmessage = (event) => {
+  if (event.data.receipt) {
+    // acknowledge message was received
+    dandi_message_received = true;
+    return
+  }
+  else if (dandi_prompting || !dandi_message_received) {
+    // do not prompt while we're already prompting or waiting
+    return;
+  }
+  // otherwise, it's a valid prompt query
+  dandi_prompting = true;
+  const dandiDialog = document.getElementById("dandi-auth-dialog");
+  const dandiLabel = document.getElementById("dandi-auth-prompt");
+  const dandiInstance = dandiLabel.getElementsByTagName("span")[0].getElementsByTagName("b")[0];
+  dandiInstance.textContent = event.data.instance.toUpperCase();
+  dandiDialog.showModal();
+};
+
+window.onbeforeunload = (event) => {
+  channel_dandi_auth.postMessage({ reset_if_undefined: true });
+};
+
+// start accepting messages from worker
+navigator.serviceWorker.startMessages();
+</script>
+"""
+
+p = ArgumentParser("Inject service worker into a html file.")
+p.add_argument('-i', '--input', default='index.html')
+p.add_argument('-o', '--output', default=None)
+p.add_argument('-s', '--script', default='sw.js')
+args = p.parse_args()
+
+code_head = code_head.replace('sw.js', args.script)
+
+with open(args.input, "rt") as f:
+  html = BeautifulSoup(f.read())
+
+html.find('head').insert(0, BeautifulSoup(code_head))
+html.find('body').insert(0, BeautifulSoup(code_body))
+
+with open(args.output or args.input, "wt") as f:
+  f.write(html.prettify())
---
+++ src/sw.js
@@ -0,0 +1,197 @@
+// --- Wayne setup ------------------------------------------------
+import { Wayne } from '/ngapp/wayne.js';
+
+const app = new Wayne();
+// ----------------------------------------------------------------
+
+app.get(`/wayne/*`, async (req, res) => {
+  console.log(`wayne: ${ req.url }`);
+});
+
+app.get(`https://github.com/{user}/{repo}`, async (req, res) => {
+  console.log(`github: ${ req.params.user }/${ req.params.repo }`);
+});
+
+app.get(`https://github.com/{user}/{repo}/*`, async (req, res) => {
+  console.log(`github: ${ req.params.user }/${ req.params.repo }`);
+});
+
+// addEventListener('install', event => {
+//     event.waitUntil(self.skipWaiting());
+// });
+
+// addEventListener('activate', event => {
+//     event.waitUntil(self.clients.claim());
+// });
+
+// --- linc & dandi utilities -------------------------------------
+const dandi_api = {
+  dandi: "https://api.dandiarchive.org/api",
+  linc: "https://api.lincbrain.org/api",
+};
+const dandi_auth = {
+  dandi: { token: null, header: {} },
+  linc: { token: null, header: {} }
+};
+const channel_dandi_auth = new BroadcastChannel('channel-dandi-auth');
+
+// Check that credentials are valid
+async function dandiCheckCredentials(instance) {
+  const api = dandi_api[instance];
+  const url = api + "/auth/token/";
+  const req = new Request(url, { headers: dandi_auth[instance].header });
+  const res = await fetch(req);
+  return res.ok;
+}
+
+function dandiAuthReset(instance) {
+  if (!instance) {
+    dandiAuthReset("dandi");
+    dandiAuthReset("linc");
+    return;
+  }
+  dandi_auth[instance].token = null;
+  dandi_auth[instance].header = {};
+}
+
+function dandiAuthResetIfDefined(instance) {
+  if (!instance) {
+    dandiAuthResetIfDefined("dandi");
+    dandiAuthResetIfDefined("linc");
+    return;
+  }
+  if (dandi_auth[instance].token) {
+    dandi_auth[instance].token = null;
+    dandi_auth[instance].header = {};
+  }
+}
+
+function dandiAuthResetIfUndefined(instance) {
+  if (!instance) {
+    dandiAuthResetIfUndefined("dandi");
+    dandiAuthResetIfUndefined("linc");
+    return;
+  }
+  if (!(dandi_auth[instance].token)) {
+    dandi_auth[instance].token = null;
+    dandi_auth[instance].header = {};
+  }
+}
+
+// Receive credentials from the main thread
+channel_dandi_auth.onmessage = async (event) => {
+  if (event.data.reset_if_undefined) {
+    // Page unloaded/reloaded -> replace undefined by null to prompt again
+    dandiAuthResetIfUndefined();
+    return;
+  }
+  const instance = event.data.instance;
+  if (event.data.token !== undefined) {
+    // Prompt response
+    dandi_auth[instance].token = event.data.token;
+    dandi_auth[instance].header = event.data.header;
+    if (!(await dandiCheckCredentials(instance))) {
+      dandiAuthReset(instance);
+    }
+  } else {
+    // Prompt canceled or empty -> store undefined to avoid asking again
+    dandi_auth[instance].token = undefined;
+  }
+  // Acknowledge reception
+  channel_dandi_auth.postMessage({ receipt: true });
+};
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+async function dandiAuth(instance) {
+  // We check for null, because undefined is interpreted as users
+  // not willing to enter credentials.
+  while (dandi_auth[instance].token === null) {
+    channel_dandi_auth.postMessage({ instance: instance });
+    await delay(500);
+  }
+  return dandi_auth[instance].header;
+}
+
+async function dandiZarrURL(api, asset_id, path, opt, instance) {
+  const url_info = `${ api }/assets/${ asset_id }/info/`;
+  let res = await fetch(new Request(url_info, opt));
+  if (res.status == 401 && dandi_auth[instance].token !== undefined) {
+    console.log("fetch -> auth");
+    dandiAuthReset(instance);
+    const header = await dandiAuth(instance);
+    res = await fetch(new Request(url_info, { headers: header }));
+  }
+  if (res.status == 401) {
+    return undefined;
+  }
+  const json = await res.json();
+  const zarr_id = json.zarr;
+  return `${ api }/zarr/${ zarr_id }/files?prefix=${ path }&download=true`;
+}
+// ----------------------------------------------------------------
+
+
+// --- capture DANDI links ----------------------------------------
+async function route_dandi_instance(req, res, instance) {
+  // Single asset (plain or zarr file) -> add auth headers and fetch
+  console.log(`route_${ instance }: ${ req.url }`);
+
+  if (dandi_auth[instance].token === null) {
+    const head = await fetch(req, { method: "HEAD" });
+    if (head.status == 401) {
+      console.log("head -> auth");
+      await dandiAuth(instance);
+    }
+  }
+
+  res.fetch(new Request(req, { headers: dandi_auth[instance].header }));
+}
+
+async function route_dandi_instance_zarr(req, res, instance) {
+  // There is something after /download/ so this is likely a zarr file
+  // -> we need to fetch the zarr if and redirect to the zarr file link
+  console.log(`route_${ instance }_zarr: ${ req.url}`);
+  const api = dandi_api[instance];
+  const asset_id = req.params.asset_id;
+  const prefix = `${ api }/assets/${ asset_id }/download/`;
+  const path = req.url.slice(prefix.length);
+
+  if (!path) {
+    return await route_dandi_instance(req, res, instance);
+  }
+  const opt = { headers: dandi_auth[instance].header };
+  const zurl = await dandiZarrURL(api, asset_id, path, opt, instance);
+  if (zurl === undefined) {
+    res.blob(null, { status: 401 });
+  } else {
+    res.redirect(zurl);
+  }
+}
+
+async function route_dandi(req, res) {
+  return await route_dandi_instance(req, res, "dandi");
+}
+
+async function route_dandi_zarr(req, res) {
+  return await route_dandi_instance_zarr(req, res, "dandi");
+}
+
+app.get(`${ dandi_api.dandi }/zarr/{zarr_id}/*`, route_dandi);
+app.get(`${ dandi_api.dandi}/assets/{asset_id}/download/*`, route_dandi_zarr);
+// ----------------------------------------------------------------
+
+
+
+// --- capture LINC links -----------------------------------------
+async function route_linc(req, res) {
+  return await route_dandi_instance(req, res, "linc");
+}
+
+async function route_linc_zarr(req, res) {
+  return await route_dandi_instance_zarr(req, res, "linc");
+}
+
+app.get(`${ dandi_api.linc }/zarr/{zarr_id}/*`, route_linc);
+app.get(`${ dandi_api.linc }/assets/{asset_id}/download/*`, route_linc_zarr);
+// ----------------------------------------------------------------
