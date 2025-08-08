51bd1f59d76e8288420da708013b655299a00939
Yaël Balbastre <yael.balbastre@gmail.com>
2025-08-08 14:34:30 +0100

    Delete src/sw2.js

--- src/sw2.js
+++
@@ -1,5 +0,0 @@
-self.addEventListener('fetch', (event) => {
-  console.log("fetch event:", event);
-  console.log("fetch url:", event.request.url);
-  event.respondWith((async () => { return await fetch(event.request) })());
-});
